2e43ee98238f5d3cd13887f31f87fb52a9b79664
SujalShakyaSeva <sujal.shakya@sevadev.com>
2022-11-29 17:27:06 +0545

    New changes

--- src/main/java/com/mystore/pageobjects/PurchaseOrder.java
+++ src/main/java/com/mystore/pageobjects/PurchaseOrder.java
@@ -14,8 +14,6 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class PurchaseOrder extends BaseClass {
-
-
 	@FindBy(xpath = "//input[@type='text']")  //Purchase Order Number
 	WebElement purchaseOrderNumber;
 
