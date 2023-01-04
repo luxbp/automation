824da46c27cf61079f74e111d526e344b65f5bee
sujal <sujal@sujals-MacBook-Pro.local>
2023-01-04 16:33:08 +0545

    Added Quick Order & fixed Order Review code

--- src/main/java/com/luxbp/pageobjects/LoginPage.java
+++ src/main/java/com/luxbp/pageobjects/LoginPage.java
@@ -16,81 +16,85 @@ import org.testng.annotations.Test;
 public class LoginPage extends BaseClass {
 
 
-	@FindBy(xpath = "//*[contains(text(),'Sign In')]")   //Click on Sign in button
-	WebElement signOn;
+    @FindBy(xpath = "//*[contains(text(),'Sign In')]")   //Click on Sign in button
+    WebElement signOn;
 
-	@FindBy(xpath = "//input[@type='email']")  //Click on Email
-	WebElement signInEmail;
+    @FindBy(xpath = "//input[@type='email']")  //Click on Email
+    WebElement signInEmail;
 
-	@FindBy(xpath = "//input[@type='password']")  //Click on Password
-	WebElement signInPassword;
+    @FindBy(xpath = "//input[@type='password']")  //Click on Password
+    WebElement signInPassword;
 
-	@FindBy(xpath = "//button[@type='submit']")  //Click on Sign in button
-	WebElement signInButton;
+    @FindBy(xpath = "//button[@type='submit']")  //Click on Sign in button
+    WebElement signInButton;
 
-	@FindBy(xpath = "//*[@data-testid='notificationMessage']")    //Verify login message
-	WebElement verifyLoginMessage;
+    @FindBy(xpath = "//*[@data-testid='notificationMessage']")    //Verify login message
+    WebElement verifyLoginMessage;
 
 
-	@FindBy(xpath = "//*[@class='ss-gizmo ss-delete text-dark text-2h6 opacity-40 hover:opacity-100 slow-color-change leading-none icon-lg']")
-	WebElement closeVerifyMessage;
+    @FindBy(xpath = "//*[@class='ss-gizmo ss-delete text-dark text-2h6 opacity-40 hover:opacity-100 slow-color-change leading-none icon-lg']")
+    WebElement closeVerifyMessage;
 
-	@FindBy(xpath = "//*[@class='toast-close']")		//Toast close
-	WebElement toastClose;
+    @FindBy(xpath = "//*[@class='toast-close']")        //Toast close
+    WebElement toastClose;
 
-	ExtentTest loginModule = extent.createTest("Login Module","This is test to validate successful login");
+    ExtentTest loginModule = extent.createTest("Login Module", "This is test to validate successful login");
 
-	public LoginPage() {
-		PageFactory.initElements(driver, this);}
-	public void clickOnsignIn() {
-		signOn.click();
-	}
-	public HomePage login(String username, String password) {
-		WebDriverWait wait = new WebDriverWait(driver,5);
-		wait.until(ExpectedConditions.visibilityOf(signInEmail));
-		Action.type(signInEmail, username);
-		Action.type(signInPassword, password);
-		return new HomePage();
-	}
+    public LoginPage() {
+        PageFactory.initElements(driver, this);
+    }
 
-	public void loginTest(String userName, String password) throws Throwable {
-		ExtentTest loginReport = extent.createTest("Login test", "This is test to validate successful login");
+    public void clickOnsignIn() {
+        signOn.click();
+    }
 
-		clickOnsignIn();
-		login(userName, password);
-		signInButton();
-		try {
-			String LogInfo = validateLogin();
+    public HomePage login(String username, String password) {
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOf(signInEmail));
+        Action.type(signInEmail, username);
+        Action.type(signInPassword, password);
+        return new HomePage();
+    }
 
-			try {
-				Assert.assertEquals("YOU ARE LOGGED IN!", LogInfo);
-				System.out.println("Login Successful");
-				loginReport.pass("Logged in successfully");
+    public void loginTest(String userName, String password) throws Throwable {
+        ExtentTest loginReport = extent.createTest("Login test", "This is test to validate successful login");
 
-			} catch (AssertionError Ae) {
-				loginReport.fail("Login failed due to " + Ae);
-				System.out.println("Logged In Failed due to " + Ae);
-			}
-		} catch (Exception logException) {
-			loginReport.fail("Login failed");
-			System.out.println("Failed due to " + logException);
-			loginReport.fail("Exception failed due to " + logException);
-		}
-		loginReport.info("Test Complete");
-	}
+        clickOnsignIn();
+        login(userName, password);
+        signInButton();
+        try {
+            String LogInfo = validateLogin();
 
-	public void signInButton() throws InterruptedException {
-		signInButton.click();
-		if (verifyLoginMessage.isDisplayed()) {
-			loginModule.pass("Successfully Logged in");
-		} else {
-			loginModule.fail("Login failed");
-		}
-	}
-	public String validateLogin(){
-		WebDriverWait waitAlter = new WebDriverWait(driver, 5);
-		waitAlter.until(ExpectedConditions.visibilityOf(closeVerifyMessage));
-		String message = verifyLoginMessage.getText();
-		return message;
-	}
+            try {
+                Assert.assertEquals("YOU ARE LOGGED IN!", LogInfo);
+                System.out.println("Login Successful");
+                loginReport.pass("Logged in successfully");
+
+            } catch (AssertionError Ae) {
+                loginReport.fail("Login failed due to " + Ae);
+                System.out.println("Logged In Failed due to " + Ae);
+            }
+        } catch (Exception logException) {
+            loginReport.fail("Login failed");
+            System.out.println("Failed due to " + logException);
+            loginReport.fail("Exception failed due to " + logException);
+        }
+        loginReport.info("Test Complete");
+    }
+
+    public void signInButton() throws InterruptedException {
+        signInButton.click();
+        if (verifyLoginMessage.isDisplayed()) {
+            loginModule.pass("Successfully Logged in");
+        } else {
+            loginModule.fail("Login failed");
+        }
+    }
+
+    public String validateLogin() {
+        WebDriverWait waitAlter = new WebDriverWait(driver, 5);
+        waitAlter.until(ExpectedConditions.visibilityOf(closeVerifyMessage));
+        String message = verifyLoginMessage.getText();
+        return message;
+    }
 }
